b3cb066ee7dd28ebb67384460b74d5dae7ee6f12
TrishaBalamurugan <trishabalamurugan2004@gmail.com>
2025-11-29 10:26:37 -0600

    Update constants.py

--- forensics/constants.py
+++ forensics/constants.py
@@ -5,13 +5,24 @@ LOGGING_KW = "logging"
 NAME_KW = "name"
 NAMES_KW = "names"
 
+# Empty string (used in parser fallback)
+EMPTY_STRING = ""
+
 # Assignment related
 TARGETS_KW = "targets"
 VALUE_KW = "value"
 
 # Function definitions / calls
-FUNC_NAME_KW = "func_name"
+FUNC_KW = "func"
 ARGS_KW = "args"
+LINE_NO_KW = "lineno"
 
-# Any other constants used by py_parser can be added here as needed
+# Attribute function calls
+PARENT_KW = "parent"
+ATTRIB_KW = "attr"
 
+# Model feature extraction
+CLASS_KW = "class"
+FEATURE_KW = "feature"
+
+# Any additional constants referenced in py_parser
